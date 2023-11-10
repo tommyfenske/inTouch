f608ab7e4c779247ac0113ec0cba23e50ce4d10a
tommyfenske <t.fenske619@gmail.com>
2023-11-09 21:51:33 -0600

    testing firebase

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 //Firebase Import and Config
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+import { getStorage, } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-storage.js";
 
 const firebaseConfig = {
-    databaseURL: "https://intouch-60715-default-rtdb.firebaseio.com"
+    databaseURL: "https://intouch-60715-default-rtdb.firebaseio.com",
+    storageBucket: "intouch-60715.appspot.com"
 };
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app)
-const photosInDB = ref(database, "photos")
+const database = getDatabase(app);
+const storage = getStorage();
+const photosInDB = ref(database, "photos");
+//const storageRef = ref(storage);
 
 //Get HTML Elements
 const fileUpload = document.getElementById("file-upload")
 const feedElement = document.getElementById("feed")
+const testP = document.getElementById("test-p")
+const testImg = document.getElementById("test-img")
 
 //On File Uploaded
 fileUpload.addEventListener("change", function(){
     let inputFile = fileUpload.value
+    //let inputFile = event.target.files[0]
+    //const photoRef = ref(storage, `images/${inputFile.name}`);
     push(photosInDB, inputFile)
-
+ 
     inputFile = ""
 })
 
@@ -28,8 +36,8 @@ function updateFeed(arr) {
     for(let i = 0; i < arr.length; i++) {
         let photoID = arr[i][0]
         let photoPath = arr[i][1]
-        let newPhoto = document.createElement("button")
-        newPhoto.innerHTML = photoPath
+        let newPhoto = document.createElement("img")
+        newPhoto.src = photoPath
         feedElement.appendChild(newPhoto)
         
         //make event listener for each button that deletes the item in the DB
